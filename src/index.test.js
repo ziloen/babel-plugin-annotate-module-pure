ffd36730323e0bc176a07e7c12bcb94bc324879b
N <34268416+ziloen@users.noreply.github.com>
2025-08-14 15:24:56 +0800

    use async transform

--- src/index.test.js
+++ src/index.test.js
@@ -1,41 +1,37 @@
+import { transformAsync } from '@babel/core'
 import test from 'node:test'
-import { transform } from '@babel/core'
 
 /**
  * @import {Options} from "./index"
  */
 
 /**
- * @param {Options} options 
- * @param {string} input 
+ * @param {Options["pureCalls"]} options
+ * @param {string} input
  * @returns {Promise<string>}
  */
-function annotatePure(options, input) {
-  return new Promise((resolve, reject) => {
-    transform(input, {
-      plugins: [
-        ["./dist/index.js", options]
-      ]
-    }, (err, result) => {
-      if (err || !result) {
-        return reject(err)
-      }
-      resolve(result.code ?? "")
-    })
+async function annotatePure(options, input) {
+  const result = await transformAsync(input, {
+    ast: false,
+    babelrc: false,
+    configFile: false,
+    plugins: [['./dist/index.js', options]],
   })
 
+  if (!result || !result.code) {
+    throw new Error('Failed to tranform')
+  }
+
+  return result.code
 }
 
-
-test("chained function", async (t) => {
+test('chained function', async (t) => {
   const input1 = `import { foo } from 'foo';\nfoo().bar();\nfoo.bar();`
   const expect = `import { foo } from 'foo';\n(/*#__PURE__*/foo()).bar();\n/*#__PURE__*/foo.bar();`
 
   const code = await annotatePure(
     {
-      pureCalls: {
-        foo: ["foo", ["foo", "bar"]]
-      }
+      foo: ['foo', ['foo', 'bar']],
     },
     input1,
   )
@@ -43,50 +39,43 @@ test("chained function", async (t) => {
   t.assert.equal(code, expect)
 })
 
-test("regular function", async (t) => {
+test('regular function', async (t) => {
   const input1 = `import { foo, bar } from 'foo';\nconst a = foo();\nbar();`
   const expect = `import { foo, bar } from 'foo';\nconst a = /*#__PURE__*/foo();\nbar();`
 
   const code = await annotatePure(
     {
-      pureCalls: {
-        foo: ["foo"]
-      }
+      foo: ['foo'],
     },
-    input1
+    input1,
   )
 
   t.assert.equal(code, expect)
 })
 
-
-test("chained new expression", async (t) => {
+test('chained new expression', async (t) => {
   const input1 = `import { Foo } from 'foo';\nnew Foo().bar();\nFoo.bar();`
   const expect = `import { Foo } from 'foo';\n(/*#__PURE__*/new Foo()).bar();\n/*#__PURE__*/Foo.bar();`
 
   const code = await annotatePure(
     {
-      pureCalls: {
-        foo: ["Foo", ["Foo", "bar"]]
-      }
+      foo: ['Foo', ['Foo', 'bar']],
     },
-    input1
+    input1,
   )
 
   t.assert.equal(code, expect)
 })
 
-test("new expression", async (t) => {
+test('new expression', async (t) => {
   const input1 = `import { Foo } from 'foo';\nconst a = new Foo();`
   const expect = `import { Foo } from 'foo';\nconst a = /*#__PURE__*/new Foo();`
 
   const code = await annotatePure(
     {
-      pureCalls: {
-        foo: ["Foo"]
-      }
+      foo: ['Foo'],
     },
-    input1
+    input1,
   )
   t.assert.equal(code, expect)
 })
